e7e7f1179aae6755eaf5976b87763459533e5edd
YootTanA <oatyootna@gmail.com>
2025-01-22 23:57:11 +0700

    write file to memory

--- dax-experiment/writer/writer.c
+++ dax-experiment/writer/writer.c
@@ -1,42 +1,69 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 int main() {
-  /* DAX mapping requires a 2MiB alignment */
-  size_t page_size = 2 * 1024 * 1024;
+  const char *filename = "your_file_name";
+  struct stat file_stat;
+  char *file_content;
 
-  int fd = open("/dev/dax0.0", O_RDWR);
-  if (fd == -1) {
+  int file_fd = open(filename, O_RDONLY);
+  if (file_fd == -1) {
+    printf("Error opening file %s\n", filename);
+    return -1;
+  }
+
+  if (fstat(file_fd, &file_stat) == -1) {
+    printf("Error getting file size");
+    close(file_fd);
+  }
+  size_t file_size = file_stat.st_size;
+
+  int dax_fd = open("/dev/dax0.0", O_RDWR);
+  if (dax_fd == -1) {
     perror("open() failed");
     return 1;
   }
 
   void *dax_addr =
-      mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+      mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, dax_fd, 0);
   if (dax_addr == MAP_FAILED) {
     perror("mmap() failed");
-    close(fd);
-    return 1;
-  }
-
-  FILE *fptr;
-  char filename[100] = "<filename>";
-
-  fptr = fopen(filename, "r");
-  if (fptr == NULL) {
-    printf("Cannot open file %s\n", filename);
+    close(file_fd);
+    close(dax_fd);
     return -1;
   }
 
-  int c = 0;
-  while ((c = fgetc(fptr)) != EOF) {
-    putchar(c);
+  char *buffer = malloc(file_size);
+  if (!buffer) {
+    printf("Error allocating buffer");
+    munmap(dax_addr, file_size);
+    close(file_fd);
+    close(dax_fd);
+    return -1;
   }
 
-  munmap(dax_addr, page_size);
-  close(fd);
+  if (read(file_fd, buffer, file_size) != file_size) {
+    printf("Error reading input file");
+    free(buffer);
+    munmap(dax_addr, file_size);
+    close(file_fd);
+    close(dax_fd);
+    return -1;
+  }
+
+  memcpy(dax_addr, buffer, file_size);
+  if (msync(dax_addr, file_size, MS_SYNC) == -1) {
+    printf("Error syncing memory");
+  }
+
+  free(buffer);
+  munmap(dax_addr, file_size);
+  close(file_fd);
+  close(dax_fd);
   return 0;
 }
